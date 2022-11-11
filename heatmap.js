49b8e9ac6fe2e68d1fbf4b6b6b273d36bac3480a
Oscar Saul <oscarsaul.mmv@gmail.com>
2022-11-10 23:19:48 -0500

    Add files via upload

---
+++ heatmap.js
@@ -0,0 +1,45 @@
+// heatmap 0.1.0 beta 
+var c = document.getElementById("screen");
+var ctx = c.getContext("2d");
+var deltaX = [];
+var heat;
+function showCoords(event) {
+  const scale = window.devicePixelRatio
+    var x = event.clientX
+    var y = event.clientY + window.scrollY;
+    var coor = "(" + x + "," + y + ")";
+    document.getElementById("coords").innerHTML = coor;
+
+
+    for (let i = 1; i <= 31; i++) {
+      var circX = event.clientX
+      var circY = event.clientY + window.scrollY;
+      ctx.fillStyle = 'rgba(0, 0, 200, 0.005)'; 
+      if(heat) {
+        ctx.fillStyle = 'rgba(200, 0, 0, 0.005)'; 
+      }
+      ctx.strokeStyle = 'transparent'; 
+      ctx.fill();
+      ctx.beginPath();
+      ctx.arc(circX, circY, i, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+    deltaX.push(circX);
+    // [100 200 320 231 342 123 355 354]
+    
+    if(Math.abs(deltaX[deltaX.length-1] - (deltaX[deltaX.length-20]) <= 50 )) {
+      heat = true;
+      deltaX = []
+    } else {
+      heat = false;
+    }
+   
+
+
+    
+  
+  function clearCoor() {
+    document.getElementById("coords").innerHTML = "";
+  }
+
+}
